2dfe4332b41a0cb1df69c1cac855da23eb094332
Daniel Price <danieljamieprice@gmail.com>
2023-09-25 23:24:14 +0900

    Return header and home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import './App.css'
+import Header from "./components/header/Header";
+import Home from "./pages/home/Home";
 
 function App() {
 
   return (
-    <h1>Hello World!</h1>
+    <>
+      <Header />
+      <Home />
+    </>
   )
 }
 
-export default App
+export default App;
