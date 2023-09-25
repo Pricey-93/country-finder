3d64e5702378cddf52225f75bda03b6da7afeb4f
Daniel Price <danieljamieprice@gmail.com>
2023-09-25 23:27:15 +0900

    Start implementing card component

---
+++ src/components/card/Card.tsx
@@ -0,0 +1,13 @@
+export default function Card() {
+
+  return (
+    <>
+      <img />
+      <h2 className="country-title">Country Title</h2>
+      <ul className="card-stats-list">
+        <ul>Population</ul>
+        <ul>Region</ul>
+      </ul>
+    </>
+  )
+}
