8a92ddf05d4d6c312ec32383a3377d6c5ea0a50a
Daniel Price <danieljamieprice@gmail.com>
2023-09-25 23:25:46 +0900

    Add search input and css

---
+++ src/components/ui/searchInput/SearchInput.tsx
@@ -0,0 +1,11 @@
+import "./SearchInput.css"
+
+export default function SearchInput() {
+
+  return (
+    <input type="search"
+    name="country-search" 
+    className="country-search-input"
+    placeholder="Search for a country..." />
+  )
+} 
