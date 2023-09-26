a6587dcc65287678add263d6a61be9f2878be7c5
Daniel Price <danieljamieprice@gmail.com>
2023-09-26 20:42:31 +0900

    Fetch data from local storage else fetch from api

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,45 @@
+import { useEffect, useRef, useState } from "react";
+import fetchAllCountries from "./api/countries";
 import Header from "./components/header/Header";
 import Home from "./pages/home/Home";
 
 function App() {
+  const countries = useRef(null);
+  const [dataLoaded, setDataLoaded] = useState(false);
+  
+  useEffect(() => {
+    (async() => {
+      const stored = localStorage.getItem("countries");
+      if (stored) {
+        countries.current = JSON.parse(stored);
+        setDataLoaded(true);
+      }
+      else {
+        const countryData = await fetchAllCountries();
+        if (countryData) {
+          localStorage.setItem("countries", JSON.stringify(countryData));
+          countries.current = countryData;
+          setDataLoaded(true);
+        }
+      }
+    })();
+  }, [])
+
+  useEffect(() => {
+    if (dataLoaded) {
+      console.log(countries.current);
+    }
+  }, [dataLoaded])
 
   return (
     <>
       <Header />
-      <Home />
+      {
+        dataLoaded 
+        ? <Home countries={countries} />
+        :
+        null
+      }
     </>
   )
 }
