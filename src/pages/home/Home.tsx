b4d7e49b39b1ab81e9f52cd915d0507ed6a3f1ff
Daniel Price <danieljamieprice@gmail.com>
2023-09-26 21:53:22 +0900

    Map countries to card components

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,15 +1,23 @@
 import "./Home.css";
+import { ICountry } from "../../../ICountry";
 import SearchInput from "../../components/ui/searchInput/SearchInput";
 import RegionFilter from "../../components/ui/regionFilter/RegionFilter";
 import Card from "../../components/card/Card";
 
-export default function Home() {
+interface IProps {
+  countries: null | ICountry[]
+}
+
+export default function Home(props: IProps) {
+  const { countries } = props;
 
   return (
     <div className="home-container">
       <SearchInput />
       <RegionFilter />
-      <Card />
+      {
+        countries?.map((country, i) => <Card country={country} key={i} />)
+      }
     </div>
   )
 }
