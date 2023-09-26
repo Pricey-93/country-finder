b3fb2a61012fcefe1f4f768506ce5925c0accbbb
Daniel Price <danieljamieprice@gmail.com>
2023-09-26 21:52:18 +0900

    Define card props

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,9 +1,15 @@
-export default function Card() {
+import { ICountry } from "../../../ICountry"
+
+interface IProps {
+  country: null | ICountry
+}
+export default function Card(props: IProps) {
+  const { country } = props;
 
   return (
     <>
       <img />
-      <h2 className="country-title">Country Title</h2>
+      <h2 className="country-title">{country?.name.common}</h2>
       <ul className="card-stats-list">
         <ul>Population</ul>
         <ul>Region</ul>
