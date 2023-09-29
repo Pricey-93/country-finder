9688c6138be5024e5b98dc86d26a9fd993577789
Daniel Price <danieljamieprice@gmail.com>
2023-09-30 02:17:09 +0900

    Implement handlers and pass props

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { useEffect, useState } from "react";
 import { ICountry } from "../ICountry";
 import fetchAllCountries from "./api/countries";
 import Header from "./components/header/Header";
+import CountryDetails from "./pages/countryDetails/CountryDetails";
 import Home from "./pages/home/Home";
 
 function App() {
   const [countries, setCountries] = useState<ICountry[]>();
+  const [activeCountry, setActiveCountry] = useState<null | ICountry>(null);
   
   useEffect(() => {
     (async() => {
@@ -29,12 +31,27 @@ function App() {
     }
   }, [countries])
 
+  function activeCountryHandler(country: ICountry): void {
+    setActiveCountry(country);
+  }
+
+  function removeCountryHandler(): void {
+    setActiveCountry(null);
+  }
+
   return (
     <>
       <Header />
       {
+        activeCountry
+        ? <CountryDetails 
+            activeCountry={ activeCountry }
+            removeCountryHandler={ removeCountryHandler }
+            activeCountryHandler={ activeCountryHandler }
+          />
+        :
         countries 
-        ? <Home countries={ countries } />
+        ? <Home countries={ countries } activeCountryHandler={ activeCountryHandler } />
         :
         null
       }
