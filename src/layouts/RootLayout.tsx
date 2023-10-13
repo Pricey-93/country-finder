3f2aebd0261a0d2d7137d20136e46dd2042084ba
Daniel Price <danieljamieprice@gmail.com>
2023-10-13 23:14:49 +0900

    Manage data with CountryManager class

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,69 +1,45 @@
-import { useState } from "react";
 import { Outlet, useOutletContext, useLoaderData } from "react-router-dom";
-import Header from "../components/header/Header";
 import { ICountry } from "../api/ICountry";
 import fetchAllCountries from "../api/countries";
-
+import CountryManager from "../api/CountryManager";
+import Header from "../components/header/Header";
 
 type ContextType = {
-  countries: ICountry[],
-  getCountries: () => ICountry[],
-  searchByName: (userInput: string) => ICountry[],
-  changeActiveCountry: (country: ICountry) => void
+  countryManager: CountryManager
 };
 
 export default function RootLayout() {
-  const countries: ICountry[] = useLoaderData() as ICountry[];
-  const [activeCountry, setActiveCountry] = useState<null | ICountry>(null);
-  const [borderCountries, setBorderCountries] = useState<null | ICountry[]>(null);
+  const countryManager = new CountryManager(useLoaderData() as ICountry[]);
 
-  function changeActiveCountry(country: ICountry): void {
-    setActiveCountry(country);
-    changeBorderCountries(country);
-  }
+  // function changeActiveCountry(country: ICountry): void {
+  //   setActiveCountry(country);
+  //   changeBorderCountries(country);
+  //   // const navigate = useNavigate();
+  //   // navigate(`countries/${activeCountry?.name.common}`);
+  // }
 
-  function changeBorderCountries(country: ICountry): void {
-    const borders = country.borders;
-    const currentBorderCountries: ICountry[] = [];
+  // function changeBorderCountries(country: ICountry): void {
+  //   const borders = country.borders;
+  //   const currentBorderCountries: ICountry[] = [];
 
-    borders?.forEach(border => {
-      const match = countries?.find(country => country.cca3 === border);
-      if (match) {
-        currentBorderCountries.push(match);
-      }
-    });
+  //   borders?.forEach(border => {
+  //     const match = countries?.find(country => country.cca3 === border);
+  //     if (match) {
+  //       currentBorderCountries.push(match);
+  //     }
+  //   });
 
-    setBorderCountries(currentBorderCountries);
-  }
-
-  function resetCountry(): void {
-    setActiveCountry(null);
-  }
-
-
-//Context methods
-function getCountries(): ICountry[] {return countries}
-
-function searchByName(userInput: string): ICountry[] {
-  const searchResult = countries?.filter(country => country.name.common.toUpperCase().includes(userInput.toUpperCase()));
-  return searchResult;
-}
+  //   setBorderCountries(currentBorderCountries);
+  // }
 
   return (
     <>
       <Header />
       <main>
-        <Outlet context={{countries, getCountries, searchByName, changeActiveCountry} satisfies ContextType}/>
+        <Outlet context={{ countryManager } satisfies ContextType}/>
       </main>
     </>
   )
-
-  // {/* <CountryDetails 
-//             activeCountry={ activeCountry }
-//             backButtonHandler={ resetCountry }
-//             borderClickHandler={ changeActiveCountry }
-//             activeBorderCountries={ borderCountries }
-//           /> */}
 
 }
 
