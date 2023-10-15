148d33f0ca028ab84543adb74ce3b79ca1e9b043
Daniel Price <danieljamieprice@gmail.com>
2023-10-16 00:28:15 +0900

    Finally finish configuring jest and RTL

--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,20 @@
-import type { Config } from "jest";
+export {};
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  moduleNameMapper: {
+    // mocking assests and styling
+    '^.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/tests/mocks/fileMock.ts',
+    '^.+\\.(css|less|scss|sass)$': '<rootDir>/tests/mocks/styleMock.ts',
 
-const config: Config = {
-  preset: "ts-jest",
-  verbose: true,
-  testEnvironment: "jest-environment-jsdom",
-  setupFilesAfterEnv: ["./jest.setup.ts"],
+    /* mock models and services folder */
+    '(assets|models|services)': '<rootDir>/tests/mocks/fileMock.ts',
+  },
+   // to obtain access to the matchers.
+  setupFilesAfterEnv: ['./jest.setup.ts'],
+      
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  modulePaths: ['<rootDir>'],
+  testEnvironment: 'jsdom',
 };
-
-export default config;
