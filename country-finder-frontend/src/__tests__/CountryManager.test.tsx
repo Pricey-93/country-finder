4c456e7beb6d81712fb9664211fc866565b0bbaf
Daniel Price <danieljamieprice@gmail.com>
2023-10-27 01:49:59 +0900

    refactor: Project structure

---
+++ country-finder-frontend/src/__tests__/CountryManager.test.tsx
@@ -0,0 +1,12 @@
+// import CountryManager from "../api/CountryManager";
+// import fetchAllCountries from "../api/countries";
+
+// beforeAll(() => {
+  
+// });
+
+// describe("CountryManager", () => {
+//   it("should return an array of countries", () => {
+//     expect(countryManager.getCountries()).toEqual(countries);
+//   });
+// });
---
+++ country-finder-frontend/src/__tests__/Header.test.tsx
@@ -0,0 +1,10 @@
+// import { render, screen } from "@testing-library/react";
+// import Header from "../components/header/Header";
+
+// describe("Header", () => {
+//   it("should have the correct title", () => {
+//     render(<Header />);
+//     const h1 = screen.getByRole("heading", {name: "Where in the world?"});
+//     expect(h1).toBeInTheDocument();
+//   });
+// });
