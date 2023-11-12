95cafabb7c0bd8d3c4de841d2dbfe828788ce3c8
Daniel Price <danieljamieprice@gmail.com>
2023-11-12 23:47:53 +0900

    test: Add test cases for country manager

---
+++ __tests__/countryManager.spec.ts
@@ -0,0 +1,22 @@
+import CountryManager from "../src/api/CountryManager";
+import fetchAllCountries from "../src/api/countries";
+
+const countries = await fetchAllCountries();
+const countryManager = new CountryManager(countries);
+const getCountriesSpy = vi.spyOn(countryManager, "getCountries");
+const getCountryByNameSpy = vi.spyOn(countryManager, "getCountryByName");
+
+describe("Country Manager", () => {
+  it("should create a country manager", ()  => {
+    expect(countryManager).toBeInstanceOf(CountryManager);
+  });
+  it("should return the list of countries", () => {
+    expect(countryManager.getCountries()).toEqual(countries);
+    expect(getCountriesSpy).toHaveBeenCalled();
+    expect(getCountriesSpy).toHaveReturnedWith(countries);
+  });
+  it("should get the given country by name", () => {
+    expect(countryManager.getCountryByName("United Kingdom")?.name.common).toBe("United Kingdom");
+    expect(getCountryByNameSpy).toHaveBeenCalled();
+  });
+})
